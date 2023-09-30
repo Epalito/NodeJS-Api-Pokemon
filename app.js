b6214efceb783adcf70fc063b87085c0e05efa3c
Tommy Epal <tommyepal@gmail.com>
2023-09-30 14:32:02 +0200

    third commit

--- app.js
+++ app.js
@@ -30,4 +30,15 @@ app.use(({res}) => {
     res.status(404).json({message})
 })
 
+//////////////////////////////////
+// permettre l'accès à l'API (CORS)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token,Origin, X-Requested-With, Content, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
